f1b9adef48035b33de5ac6a5fe63bca198fea1eb
superranchina <62977326+superranchina@users.noreply.github.com>
2024-02-19 09:43:53 -0800

    up

--- week3/D3/note/react_note/src/components/App.jsx
+++ week3/D3/note/react_note/src/components/App.jsx
@@ -1,29 +1,34 @@
-import useUpdatingClock from 'hooks/useUpdatingClock'
+
 
 export default function App() {
-  const name = <h1>Ran</h1>;
-  const age = <h2>23</h2>;
-  const email = <h2> liuran0614@gamil.com</h2>;
+  // const name = <h1>Ran</h1>;
+  // const age = <h2>23</h2>;
+  // const email = <h2> liuran0614@gamil.com</h2>;
 
-  const user = (
-    <div>
-      {name}
-      {age}
-      {email}
-    </div>
+  // const user = (
+  //   <div>
+  //     {name}
+  //     {age}
+  //     {email}
+  //   </div>
+  // );
+  // return (
+  //   <div className='App'>
+  //     {user}
+  //   </div>
+  // )
+  const age = 15;
+  return ( <div className="App">
+    {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+  </div>
   );
-  return (
-    <div className='App'>
-      {user}
-    </div>
-  )
 };
 
 
-const GetName = () => {
-  return "Ran";
-};
+// const GetName = () => {
+//   return "Ran";
+// };
 
-const GetNameComp = () => {
-  return <h1>Ran</h1>;
-}
+// const GetNameComp = () => {
+//   return <h1>Ran</h1>;
+// }
